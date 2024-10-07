e64235764fecd02f347b5ca88ed836247908dfde
jay22mehta <jay22mehta@gmail.com>
2024-10-07 14:58:29 +0200

    data transformation done

--- src/exception.py
+++ src/exception.py
@@ -12,11 +12,23 @@ def error_message_detail(error,error_detail:sys):
 
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error.message=error_message_detail(error_message,error_detail=error_detail)
+        # Store the detailed error message in an attribute
+        self.error_message = self.error_message_detail(error_message, error_detail)
 
+    def error_message_detail(self, error_message, error_detail: sys):
+        """
+        Create a detailed error message including the file name, line number,
+        and the original error message.
+        """
+        _, _, exc_tb = error_detail.exc_info()  # Get traceback
+        file_name = exc_tb.tb_frame.f_code.co_filename  # Get file name
+        line_number = exc_tb.tb_lineno  # Get line number
 
+        # Format the error message
+        detailed_message = f"Error occurred in script: {file_name}, line: {line_number}, error: {str(error_message)}"
+        return detailed_message
 
     def __str__(self):
-        return self.error_message 
+        return self.error_message  # Return the detailed error message when printing the exception
